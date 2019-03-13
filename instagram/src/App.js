3cfc4b4c696ea8f724c5d3649def9436109eefb2
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-13 18:53:50 -0400

    Imported boilerplate searchbar into app.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -13,9 +14,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-       
-        </header>
+        <div className= "container">
+        <SearchBar />
+       </div>
       </div>
     );
   }
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return <h1> I am Search Bar! </h1>
+}
+
+export default SearchBar;
