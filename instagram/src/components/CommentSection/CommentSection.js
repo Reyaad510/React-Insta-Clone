01a6caaa049e6ba2c4fdf5f777c95b0e12d292f6
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-15 15:45:52 -0400

    created changeHandler in commentsection component

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
-import Comment from '../Comment.js/Comment';
-import './CommentSection.css'
+import React, { Component } from "react";
+import Comment from "../Comment.js/Comment";
+import "./CommentSection.css";
 
 class CommentSection extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            comments: props.comments
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.comments,
+      text: ''
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                {this.state.comments.map(comment => {
-                    return <Comment comment={comment} key={comment.text} />
-                })}
-                 <div className='comment-input-box'>
-                    <input className='comment-input' placeholder="Add Comment..." />
-                    </div>
-            </div>
-        );
+  changeHandler = event => {
+      this.setState({ text: event.target.value })
+  }
 
+ 
+
+  render() {
+    return (
+      <div>
+        {this.state.comments.map(comment => {
+          return <Comment comment={comment} key={comment.text} />;
+        })}
+        <form className="comment-input-box">
+          <input 
+          className="comment-input" 
+          placeholder="Add Comment..."
+          value={this.state.text}
+          onChange={this.changeHandler}  />
+        </form>
+      </div>
+    );
   }
 }
 
