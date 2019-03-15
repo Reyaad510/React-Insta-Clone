b5f595edd508d8013910d47568e95b40bc8417b0
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-15 18:38:49 -0400

    Made addLike component into a toggle to add/minus a like with css change

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,54 +1,69 @@
-import React, {Component} from 'react';
-import './PostContainer.css';
-import CommentSection from '../CommentSection/CommentSection';
+import React, { Component } from "react";
+import "./PostContainer.css";
+import CommentSection from "../CommentSection/CommentSection";
 
-class PostContainer extends Component{
+class PostContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: props.data,
-      likes: props.data.likes
+      likes: props.data.likes,
+      updated: false
+    };
+  }
+
+
+  addLikes = () => {
+    if(!this.state.updated) {
+      this.setState(prevState => {
+        return {
+          likes: prevState.likes + 1,
+          updated: true
+        };
+      });
+    } else {
+      this.setState(prevState => {
+        return {
+          likes: prevState.likes - 1,
+          updated: false
+        }
+      })
     }
   }
 
-  addLikes = () => {
-   console.log(this.state.likes)
-   this.setState(prevState => {
-     return {
-       likes: prevState.likes + 1
-     }
-   })
-  }
 
   render() {
-  return (
-    <div className="post">
-      <div className="top-post">
-        <img className="imgUrl" src={this.state.data.thumbnailUrl} alt="img1" />
-        <p className="bold">{this.state.data.username}</p>
-      </div>
-      <img className="mid-img" src={this.state.data.imageUrl} alt="img2" />
+    let red = this.state.updated ? 'red' : null;
+    return (
+      <div className="post">
+        <div className="top-post">
+          <img
+            className="imgUrl"
+            src={this.state.data.thumbnailUrl}
+            alt="img1"
+          />
+          <p className="bold">{this.state.data.username}</p>
+        </div>
+        <img className="mid-img" src={this.state.data.imageUrl} alt="img2" />
 
-      <div onClick={this.addLikes}>
-      <div className="like-comment-symbols">
-        <p> 
-          <i class="far fa-heart fa-lg" />
-        </p>
-        <p>
-          <i class="far fa-comment fa-lg" />
-        </p>
-      </div>
-      <div className="likes">
-        <p className="bold">{this.state.likes} likes</p>
-      </div>
-      </div>
+        <div className="like-comment-symbols">
+          <p className={`${red}`} onClick={this.addLikes}>
+            <i class="far fa-heart fa-lg" />
+          </p>
+          <p>
+            <i class="far fa-comment fa-lg" />
+          </p>
+        </div>
+        <div className="likes">
+          <p className="bold">{this.state.likes} likes</p>
+        </div>
 
-      <div>
-        <CommentSection comments={this.state.data.comments} />
+        <div>
+          <CommentSection comments={this.state.data.comments} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
 }
 
 export default PostContainer;
