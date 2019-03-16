224710683df3a9fa2775e713ab5e8fc9f1e28c2e
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-15 22:03:17 -0400

    Search bar functionality created. MVP done

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
     super();
     this.state = {
       dummyData: [],
+      searchData: [],
+      search: ''
+
     }
   }
 
@@ -19,18 +22,31 @@ class App extends Component {
     this.setState({ dummyData: dummyData })
   }
 
+  searchChangeHandler = (event) => {
+    this.setState({ search: event.target.value.substr(0,20) })
+  }
+
+ 
+
  
 
   render() {
+    let filteredContacts = this.state.dummyData.filter(data => {
+      return data.username.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+    })
     return (
       <div className="App">
         <div className= "container">
-        <SearchBar />
-          {this.state.dummyData.map(data => {
+        <SearchBar
+         search={this.state.search}
+         searchChangeHandler={this.searchChangeHandler} />
+
+          {filteredContacts.map(data => {
             return <PostContainer 
             data={data} 
             key={data.username}
              />
+
           })}
        </div>
       </div>
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './SearchBar.css';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div className="search-bar">
       <div className="search-left">
@@ -11,7 +11,10 @@ const SearchBar = () => {
       <p> Instagram </p>
       </div>
       <div className="search-mid">
-        <input placeholder="&#128269; Search" />
+        <input 
+        value={props.search}
+        onChange={props.searchChangeHandler}  
+        placeholder="&#128269; Search" />
       </div>
       <div className="search-right">
         <i class="far fa-compass fa-lg" />
