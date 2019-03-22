8dd76dcebedbab2ca073429c190e34654be3a8fd
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-21 22:09:28 -0400

    Refactored Login.js with styled components. MVP complete

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,48 +1,71 @@
 import React from "react";
 import "./Login.css";
+import styled from "styled-components";
+import Button from '../ReusableStyles/Button';
+
+// Styled Components
+
+const LoginInput = styled.input`
+  border: 0;
+  padding: 12px 16px;
+  margin: 8px;
+  background: #fff;
+  font-size: 1rem;
+  width: 240px;
+  border-radius: 0;
+  outline: 0;
+  border: 1px solid #d4d4d4;
+  border-radius: 6px;
+`;
+
+
+
+
+
 
 class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      username:'',
-      password:''
+      username: "",
+      password: ""
     };
   }
 
   inputChangeHandler = event => {
-      this.setState({ [event.target.name]: event.target.value })
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   login = () => {
-      localStorage.setItem('username', this.state.username);
-      localStorage.setItem('password', this.state.password);
-     
-     
-  }
-
-
+    localStorage.setItem("username", this.state.username);
+    localStorage.setItem("password", this.state.password);
+  };
 
   render() {
     return (
-      <form className='login'>
-          <p> Instagram </p>
-        <input
-         name="username"
-         value={this.state.username} placeholder="Username"
-         className='login-input'
-         onChange={this.inputChangeHandler} />
+      <form className="login">
+        <p> Instagram </p>
+        <LoginInput
+          name="username"
+          value={this.state.username}
+          placeholder="Username"
+          className="login-input"
+          onChange={this.inputChangeHandler}
+        />
 
-        <input 
-        name="password"
-        value={this.state.password} placeholder='Password' 
-        className='login-input'
-        onChange={this.inputChangeHandler} />
-        
-        <button onClick={this.login}>Login</button>
+        <LoginInput
+          name="password"
+          value={this.state.password}
+          placeholder="Password"
+          className="login-input"
+          onChange={this.inputChangeHandler}
+        />
+
+        <Button onClick={this.login}>Login</Button>
       </form>
     );
   }
 }
 
 export default Login;
+
--- instagram/src/components/PostContainer/PostPage.js
+++ instagram/src/components/PostContainer/PostPage.js
@@ -1,26 +1,32 @@
 import React from "react";
 import PostContainer from "./PostContainer";
-import SearchBar from '../SearchBar/SearchBar';
+import SearchBar from "../SearchBar/SearchBar";
+import Button from "../ReusableStyles/Button";
+import styled from "styled-components";
+
+const DivCenter = styled.div`
+  text-align: center;
+  margin: 50px 0;
+`;
 
 const PostPage = props => {
   let filteredContacts = props.data.filter(data => {
     return (
-      data.username.toLowerCase().indexOf(props.search.toLowerCase()) !==
-      -1
+      data.username.toLowerCase().indexOf(props.search.toLowerCase()) !== -1
     );
   });
   return (
     <div>
-          <SearchBar
-          search={props.search}
-          searchChangeHandler={props.searchChangeHandler}
-        />
+      <SearchBar
+        search={props.search}
+        searchChangeHandler={props.searchChangeHandler}
+      />
       {filteredContacts.map(data => {
         return <PostContainer data={data} key={data.username} />;
       })}
-        <div className='logout'>
-          <button onClick={props.logOut}> LogOut </button>
-          </div>
+      <DivCenter>
+        <Button onClick={props.logOut}> LogOut </Button>
+      </DivCenter>
     </div>
   );
 };
