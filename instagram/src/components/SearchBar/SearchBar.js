8c712b0b03de6ff942389e750c5c22727880f4a4
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-21 21:02:27 -0400

    Imported styledcomponents and refactored SearchBar with it

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,24 +1,98 @@
 import React from "react";
 import './SearchBar.css';
+import styled, { css } from 'styled-components';
+
+// Styled Components
+
+const SearchBarDiv = styled.div`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  background-color: whitesmoke;
+  width: 100%;
+  top: 0;  
+`;
+
+const SearchLeft = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 8%;
+
+
+  @media(max-width: 650px){
+    margin-left: 0%;
+  }
+`;
+
+const Icon = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex:1;
+  padding-right: 10px;
+  cursor: pointer;
+`;
+
+const Instagram = styled.div`
+  padding-left: 10px;
+  flex: 1;
+  cursor: pointer;
+  font-size: 40px;
+  font-family: 'Cookie', cursive;
+  line-height: 0;
+
+  @media(max-width: 650px) {
+    font-size: 30px;
+  }
+`;
+
+const SearchMid = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left: 7%;
+  flex: 1 1 100%;
+
+  input {
+    font-size: 16px;
+    outline: none;
+    width: 80%;
+
+    @media(max-width: 650px) {
+      width: 100%;
+    }
+
+    ::placeholder {
+      text-align: center;
+    }
+  }
+`;
+
+const SearchRight = styled.div`
+  display:flex;
+  justify-content: center;
+  flex: 1 1 60%;
+`;
+
+
 
 const SearchBar = (props) => {
   return (
-    <div className="search-bar">
-      <div className="search-left">
-        <div className='icon'>
+    <SearchBarDiv>
+      <SearchLeft>
+        <Icon>
         <i class="fab fa-instagram fa-2x" />
-      </div>
-      <div className='instagram'>
+      </Icon>
+      <Instagram>
       <p> Instagram </p>
-      </div>
-      </div>
-      <div className="search-mid">
+      </Instagram>
+      </SearchLeft>
+      <SearchMid>
         <input 
         value={props.search}
         onChange={props.searchChangeHandler}  
         placeholder="&#128269; Search" />
-      </div>
-      <div className="search-right">
+      </SearchMid>
+      <SearchRight>
       <p>
         <i class="far fa-compass fa-lg" />
         </p>
@@ -28,8 +102,8 @@ const SearchBar = (props) => {
         <p>
         <i class="far fa-user fa-lg" />
         </p>
-      </div>
-    </div>
+      </SearchRight>
+    </SearchBarDiv>
   );
 };
 
